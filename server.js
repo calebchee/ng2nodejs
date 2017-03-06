34ac21854a2874bf8d206dc0457bf1ef9632853f
calebchee <GuanHao.Chee@Chubb.com>
2017-03-06 14:33:44 +1100

    second

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/setup', function(req, res) {
 // =======================
 // basic route
 app.get('/', function(req, res) {
-    res.send('Hello! The API is at http://localhost:' + port + '/api');
+    res.send('Wrong Way. Go Home.');
 });
 
 // API ROUTES -------------------
